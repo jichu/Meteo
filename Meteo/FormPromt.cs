b8b781fc8543825c2742a29d2beef0ccc987b108
jichu <zak.roman4@gmail.com>
2019-01-17 21:38:03 +0100

    výstup, koncept (ješttě in progress) bez db

---
+++ Meteo/FormPromt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Meteo
+{
+    public partial class FormPromt : Form
+    {
+        internal string Value { get; set; }
+
+        public FormPromt(PromtData pd)
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Width = pd.Width;
+            labelTitle.Text = pd.Title + ":";
+            textBoxValue.Text = pd.Value;
+            buttonOK.Text = pd.OK;
+        }
+        
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            Value = textBoxValue.Text;
+            Close();
+        }
+    }
+
+    public class PromtData
+    {
+        public string Title { get; set; }
+        public string Value { get; set; }
+        public string OK { get; set; }
+        public int Width { get; set; }
+    }
+
+}
--- Meteo/UserControlOutput.cs
+++ Meteo/UserControlOutput.cs
@@ -16,6 +16,7 @@ namespace Meteo
     {
         private static UserControlOutput uc;
         private PictureBox canvas;
+        private List<string> settingOutputVarriableNames = new List<string>();
 
         public static UserControlOutput Instance
         {
@@ -31,8 +32,17 @@ namespace Meteo
         {
             InitializeComponent();
             CreateCanvas();
-
+            CreateTable();
             Render();
+            //Util.SetSettings("pokus", "kk");
+        }
+
+        private Color GetOutputColor(int v)
+        {
+            string colorStr = Util.GetSettings($"output_result{v}_color");
+            if (colorStr ==null)
+                return Color.White;
+            return ColorTranslator.FromHtml(colorStr);
         }
 
         private void CreateCanvas()
@@ -48,20 +58,84 @@ namespace Meteo
             canvas.Paint += Canvas_Paint;
         }
 
-        private void Render()
+        private void CreateTable()
         {
+            dgv.Columns.Add("Id", "");
+            dgv.Columns["Id"].Width = 0;
+            dgv.Columns.Add("Color", "");
+            dgv.Columns["Color"].Width = 15;
+            dgv.Columns.Add("Region", "Region");
+            dgv.Columns.Add("Value", "Hodnota");
+            dgv.Rows.Clear();
+            //dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellDoubleClick);
+            dgv.ClearSelection();
+        }
+
+        private void dgv_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            FormPromt promt = new FormPromt(new PromtData() {
+                Title="Barva výstupní hodnoty pro "+ (sender as DataGridView).Rows[e.RowIndex].Cells["Value"].Value,
+                Value= Color.FromName((sender as DataGridView).Rows[e.RowIndex].Cells["Color"].Style.BackColor.Name).ToArgb().ToString(),
+                OK="Uložit",
+                Width=200
+            });
+            promt.ShowDialog();
+            Util.l(promt.Value);
+        }
+
+        private void AddTable(DataOutput data)
+        {
+            DataGridViewRow row;
+            DataGridViewCell CellByName(string columnName)
+            {
+                var column = dgv.Columns[columnName];
+                if (column == null)
+                    throw new InvalidOperationException("Unknown column name: " + columnName);
+                return row.Cells[column.Index];
+            }
+            row = new DataGridViewRow();
+            row.CreateCells(dgv);
+            DataGridViewCellStyle cellStyle = new DataGridViewCellStyle();
+            cellStyle.SelectionBackColor = Color.Purple;
+            cellStyle.SelectionForeColor = Color.White;
+            CellByName("Color").Style.BackColor = data.Color;
+            CellByName("Region").Value = data.RegionName;
+            CellByName("Value").Value = data.Value;
+            row.ReadOnly = true;
+            dgv.Rows.Add(row);
+
+        }
+
+        internal void Render()
+        {
+            Draw(new Dictionary<string, float>()
+            {
+                { "Aš", 0 },
+                { "Tachov",1 },
+                { "Kraslice",2 },
+                { "Ostrov",3}
+            });
+            DataGridViewSelectionMode oldmode = dgv.SelectionMode;
             canvas.Invalidate();
+        }
+
+        private void Draw(Dictionary<string, float> output)
+        {
+            foreach (var ll in output)
+            {
+                AddTable(new DataOutput()
+                {
+                    Color = GetOutputColor((int)ll.Value),
+                    RegionName = ll.Key,
+                    Value = ll.Value
+                });
+                DrawRegion(Model.Cloud.ORPNameToColor(ll.Key), GetOutputColor((int)ll.Value));
+            }
         }
 
         private void Canvas_Paint(object sender, PaintEventArgs e)
         {
-
-            List<CloudORPColor> l = Model.Cloud.ORPColorGetORPColors();
-            foreach (var ll in l)
-            {
-                DrawRegion(ll.color, Color.Red);
-            }
-            //DrawRegion("Beroun", Color.Red);
 
         }
 
@@ -75,10 +149,10 @@ namespace Meteo
                 foreach (JArray point in JsonConvert.DeserializeObject<JArray>(coods))
                 {
                     ((Bitmap)canvas.Image).SetPixel((int)point[0], (int)point[1], value);
-
                 }
-
             }
         }
+
+
     }
 }
