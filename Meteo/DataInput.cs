395808d0662a016141fd47c89a0ea4caa12543fd
Jaromir Svejda <jaromir.svejda@gmail.com>
2019-05-18 12:56:17 +0200

    DataMask

--- Meteo/DataInput.cs
+++ Meteo/DataInput.cs
@@ -9,14 +9,13 @@ namespace Meteo
     internal class DataInput
     {
         public string ModelName { get; set; }
-        public string SubmodelName { get; set; }
+        public Dictionary <string, List<DataSpectrum> > SubmodelSpectrum { get; set; }
         public List<DataMask> Mask { get; set; }
-        public List<DataSpectrum> Spectrum { get; set; }
     }
 
     internal class DataMask
     {
-        public string ORP { get; set; }
+        public string Color { get; set; }
         public string Coods { get; set; }
     }
 
