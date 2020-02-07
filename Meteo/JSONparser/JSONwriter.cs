0e292ccea62bc78ef30a68daf2a1731f945a9856
jichu <zak.roman4@gmail.com>
2020-02-07 16:41:17 +0100

    new json parser (příprava) přidáný nový logovací mechanismus

---
+++ Meteo/JSONparser/JSONwriter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meteo.JSONparser
+{
+    class JSONwriter
+    {
+        internal async Task Do()
+        {
+            var task = Task.Run(() => SaveToFile());
+            await task;
+        }
+
+        private bool SaveToFile()
+        {
+            try
+            {
+                int b = 0 * 9;
+                int a = 10 / b;
+                Console.WriteLine(a);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Utils.Log.Error(e, this.GetType().Name);
+                return false;
+            }
+        }
+    }
+}
---
+++ Meteo/Utils/Log.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meteo.Utils
+{
+    internal static class Log
+    {
+        private static string pathLog = "log";
+        private static string format = "yyMMdd_HHmmss";
+        private static string ext = ".log";
+        private static void CreatePathLog()
+        {
+            if (!Directory.Exists(pathLog))
+                Directory.CreateDirectory(pathLog);
+        }
+
+        internal static void Error(Exception ex, string message="")
+        {
+            try
+            {
+                CreatePathLog();
+                string path = Path.Combine(pathLog,$"err_{DateTime.Now.ToString(format)}{ext}");
+                Console.WriteLine($"Error -> {path}");
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    if (message != "")
+                        sw.WriteLine(message);
+                    sw.Write(ex.ToString() + Environment.NewLine+Environment.NewLine);
+                    sw.WriteLine(ex.Message);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Chyba chyby {e.Message}");
+            }
+        }
+    }
+}
