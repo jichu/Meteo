f856ea567eaacb2a1f7e187e5d3537c2a9db09e6
jichu <zak.roman4@gmail.com>
2020-02-27 15:11:21 +0100

    nový projekt MeteoViewer

---
+++ MeteoViewer/JSONparser/JSONreader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeteoViewer.JSONparser
+{
+    internal static class JSONreader
+    {
+
+    }
+}
---
+++ MeteoViewer/JSONparser/JSONwriter.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeteoViewer.JSONparser
+{
+    internal static class JSONwriter
+    {
+        internal static string PathJson { get; set; } = "export";
+        private static string fienamePrefix = "";
+        private static string format = "yyMMdd_HHmmss";
+        private static string ext = ".json";
+        private static JObject JData;
+
+        internal static string CreateJsonFilename(string name="")
+        {
+            CreatePath();
+            if (name == string.Empty)
+                name = $"{DateTime.Now.ToString(format)}{ext}";
+            else
+                name += ext;
+            return Path.Combine(PathJson, fienamePrefix+name);
+        }
+        internal static void Add(string prop, JArray values)
+        {
+            if (JData == null) JData = new JObject();
+            JData.Add(
+                new JProperty(prop, values)
+            );
+        }
+        internal static void Clear()
+        {
+            JData = null;
+        }
+
+        internal static void CreateJson(JObject data = null, string fPrefix = "")
+        {
+            fienamePrefix = fPrefix;
+            _ = Do(data ?? JData, "");
+        }
+        internal static void CreateJsonRoot(JObject data = null, string filename="root")
+        {
+            _ = Do(data ?? JData, filename);
+        }
+        internal async static Task Do(dynamic data, string name="")
+        {
+            await Task.Run(() => SaveToFile(data, name));
+        }
+        private static void CreatePath()
+        {
+            if (!Directory.Exists(PathJson))
+                Directory.CreateDirectory(PathJson);
+        }
+
+        private static bool SaveToFile(dynamic data, string filename)
+        {
+            try
+            {
+                using (StreamWriter file = File.CreateText(JSONwriter.CreateJsonFilename(filename)))
+                using (JsonTextWriter writer = new JsonTextWriter(file))
+                {
+                    data.WriteTo(writer);
+                }
+                /*
+                CreatePath();
+                using (StreamWriter file = File.CreateText(Path.Combine(PathJson, filename)))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, data);
+                }
+                */
+                return true;
+            }
+            catch (Exception e)
+            {
+                Utils.Log.Error(e, "SaveToFile");
+                return false;
+            }
+        }
+    }
+}
