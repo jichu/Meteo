25725fb7e38b8481b4745465c0c62c58f30c1cbc
Jaromir Svejda <Jaromir@DESKTOP-N17U6SJ>
2022-03-02 16:23:40 +0100

    Model.cs

---
+++ Meteo_2/Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Meteo
+{
+    public static class Model
+    {
+        public static DataAccess Cloud = new DataAccess();
+
+        public static string ConnStr(string name)
+        {
+            string connstr = @"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename={AppDir}Cloud3.mdf;Integrated Security=True;Connect Timeout=30";
+            //var connstr = ConfigurationManager.ConnectionStrings["Cloud"].ConnectionString;
+            if (Util.Develop)
+                return connstr.Replace("{AppDir}", AppDomain.CurrentDomain.BaseDirectory.Replace("\\bin\\Debug", ""));
+                //return ConfigurationManager.ConnectionStrings[name].ConnectionString.Replace("{AppDir}", AppDomain.CurrentDomain.BaseDirectory.Replace("\\bin\\Debug",""));
+            else
+                return ConfigurationManager.ConnectionStrings[name].ConnectionString.Replace("{AppDir}", AppDomain.CurrentDomain.BaseDirectory);
+        }
+
+    }
+}
